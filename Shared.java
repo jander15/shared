724d8a16d1657369e80369240174f1f628eef406
josh anderson <janderson@aspenk12.net>
2020-07-16 16:40:44 -0600

    wrote static method in shared class

--- Shared.java
+++ Shared.java
@@ -1,4 +1,9 @@
 package org.firstinspires.ftc.teamcode.shared;
 
 public class Shared {
+
+        public static void foo(){
+            System.out.println("this method is shared!");
+        }
+
 }
